7f133b454c8690a3f98584ef5bc3aeef5e969d01
regor <ehog@ehog.hu>
2018-12-11 17:06:55 +0100

    Interface

---
+++ src/main/java/com/bence/ujj/kotlindemo/interface.kt
@@ -0,0 +1,26 @@
+import java.util.*
+
+interface ICallable {
+    val phoneNumber: String
+    fun call()
+    fun greet() = call()
+}
+
+interface IUser {
+    fun greet() = println("hello")
+    val name: String
+    val birthDate: Date
+    val gender: Gender
+}
+
+data class User(override val name: String, override val birthDate: Date,
+                override val gender: Gender, override val phoneNumber: String) : IUser, ICallable {
+    override fun call() {
+        super<ICallable>.greet()
+    }
+
+    override fun greet() = println("Hello, $name")
+}
+
+
+
