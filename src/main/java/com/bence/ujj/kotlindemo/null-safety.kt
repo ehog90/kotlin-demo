37b167f383b67bb31dfc86a23bec01dd14b58ca1
regor <ehog@ehog.hu>
2018-12-12 09:37:49 +0100

    Null safety and safe casts

---
+++ src/main/java/com/bence/ujj/kotlindemo/null-safety.kt
@@ -0,0 +1,21 @@
+package com.bence.ujj.kotlindemo
+
+fun main(args: Array<String>) {
+    var someInt = 0;
+    var anotherInt: Int = 0
+    someInt = null // Compilation error
+    anotherInt = null // Compilation error
+    var nullableString: String? = "test"
+    nullableString = null
+
+    //null checking 1.
+    val notNullString: String  = if (nullableString == null) "" else nullableString
+    //null checking 2.
+    val notNullString2: String  = nullableString ?: ""
+
+    //safe calls
+    val stringLength: Int? = nullableString?.length
+
+    //NPE: If the expression is null, it will throw an NPE.
+    var stringLength2 = nullableString!!.substring(0,1)
+}
---
+++ src/main/java/com/bence/ujj/kotlindemo/typechecking.kt
@@ -0,0 +1,15 @@
+package com.bence.ujj.kotlindemo
+
+import java.lang.Exception
+
+fun main(args: Array<String>) {
+    reverse("morgan")
+}
+
+fun reverse(obj: Any): String {
+    if (obj is String) {
+        return obj.reversed()
+    }
+    throw Exception("It's not a string!")
+
+}
