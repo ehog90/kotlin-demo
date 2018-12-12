157a4fd0eb5002696dbc8bc70108abdbe8f1226d
regor <ehog@ehog.hu>
2018-12-12 09:00:19 +0100

    Singleton example

---
+++ src/main/java/com/bence/ujj/kotlindemo/singletons.kt
@@ -0,0 +1,22 @@
+import java.util.*
+
+object singleton {
+    var anyProperty: Int = 1
+    val immutableProperty: Int = 4
+    fun anyFunction() {
+
+    }
+}
+interface IStat {
+    val usersCreatedByGender: Map<Gender, Int>
+}
+class UserWithCompanionObject(override val name: String,
+                              override val birthDate: Date,
+                              override val gender: Gender) : IUser {
+    companion object {
+        var usersCreated = 0
+    }
+    object genderStatistics : IStat {
+        override val usersCreatedByGender = mapOf<Gender, Int>()
+    }
+}
